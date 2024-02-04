8307652ae80a2468d1364cd8e7b84d7d154d1d98
TwilightLogic <twilightlogic@gmail.com>
2024-02-04 16:27:25 +0800

    feat: update the style on dark mode

--- src/dashboard/components/sidebar.tsx
+++ src/dashboard/components/sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarRoutes } from "./sidebar-routes";
 
 export const Sidebar = () => {
   return (
-    <div className="h-full border-r border-zinc-200/40 flex flex-col overflow-y-auto ">
+    <div className="h-full border-r border-zinc-200/40 dark:border-zinc-700/80 flex flex-col overflow-y-auto ">
       <div className="h-[80px] flex items-center justify-start p-6">
         <Logo />
       </div>
