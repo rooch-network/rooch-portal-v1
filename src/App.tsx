6ff72372fdca333345d2dff8d4431b925a7bac2b
TwilightLogic <twilightlogic@gmail.com>
2024-02-04 15:37:35 +0800

    feat: complete the mode toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { DashboardLayout } from "./dashboard/dashboard-layout";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <div>
-      <DashboardLayout />
-    </div>
+    <>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <DashboardLayout />
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/dashboard/components/navbar.tsx
+++ src/dashboard/components/navbar.tsx
@@ -1,7 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export const Navbar = () => {
   return (
     <div className="p-4 border-b border-zinc-200/40 h-full flex items-center">
-      Navbar
+      <div className="flex items-center justify-end w-full gap-x-2">
+        {/* Mode Toggle */}
+        <ModeToggle />
+        {/* Wallet Connect */}
+        <div>Connect Wallet</div>
+      </div>
     </div>
   );
 };
