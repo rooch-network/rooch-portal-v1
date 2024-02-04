cdf62c85466599fc6d19ff568a27247a69aa8f24
TwilightLogic <twilightlogic@gmail.com>
2024-02-04 18:26:03 +0800

    feat: update some styles

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,7 +15,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="border-zinc-200/40 dark:border-zinc-700/80 dark:hover:bg-zinc-700/80"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -23,19 +27,19 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          <div className="flex items-center justify-start gap-x-1">
+          <div className="flex items-center justify-start gap-x-2">
             <Sun className="h-[1rem] w-[1rem] rotate-0 transition-all" />
             Light
           </div>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <div className="flex items-center justify-start gap-x-1">
+          <div className="flex items-center justify-start gap-x-2">
             <Moon className="h-[1rem] w-[1rem] rotate-0 transition-all " />
             Dark
           </div>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          <div className="flex items-center justify-start gap-x-1">
+          <div className="flex items-center justify-start gap-x-2">
             <Settings className="h-[1rem] w-[1rem] rotate-0 transition-all" />
             System
           </div>
