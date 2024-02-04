e39e2cf4c350e71c1e157f53e325e17cd99c8e68
TwilightLogic <twilightlogic@gmail.com>
2024-02-04 15:37:35 +0800

    feat: complete the mode toggle functionality

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,46 @@
+import { Moon, Settings, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "@/components/theme-provider";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <div className="flex items-center justify-start gap-x-1">
+            <Sun className="h-[1rem] w-[1rem] rotate-0 transition-all" />
+            Light
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <div className="flex items-center justify-start gap-x-1">
+            <Moon className="h-[1rem] w-[1rem] rotate-0 transition-all " />
+            Dark
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <div className="flex items-center justify-start gap-x-1">
+            <Settings className="h-[1rem] w-[1rem] rotate-0 transition-all" />
+            System
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
