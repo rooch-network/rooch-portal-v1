68d3a47441d79b19f8118946e884ab37606c175c
TwilightLogic <twilightlogic@gmail.com>
2024-02-04 14:30:58 +0800

    feat: change the layout into fiexd style of dashboard

--- src/dashboard/components/logo.tsx
+++ src/dashboard/components/logo.tsx
@@ -1,6 +1,12 @@
 export const Logo = () => {
   return (
     // Change the png to svg
-    <img src="/rooch_black_combine.png" alt="Logo" width={130} height={130} />
+    <img
+      src="/rooch_black_combine.svg"
+      alt="Logo"
+      height={120}
+      width={120}
+      className="cursor-pointer"
+    />
   );
 };
