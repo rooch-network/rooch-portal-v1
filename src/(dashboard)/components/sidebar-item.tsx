2a7509c8a90222171dadae7705444137830ade1a
TwilightLogic <twilightlogic@gmail.com>
2024-02-08 18:10:34 +0800

    refactor: update the layouts of navbar and main content

--- src/(dashboard)/components/sidebar-item.tsx
+++ src/(dashboard)/components/sidebar-item.tsx
@@ -32,58 +32,27 @@ export const SidebarItem = ({
   };
 
   return (
-    // <button
-    //   onClick={onClick}
-    //   type="button"
-    //   className={cn(
-    //     "flex items-center gap-x-2 text-zinc-500 dark:text-white text-sm font-[500] pl-6 transition-all hover:text-zinc-600 dark:hover:text-white hover:bg-zinc-300/20 dark:hover:bg-zinc-900",
-    //     isActive &&
-    //       "text-zinc-700 dark:text-white bg-zinc-200/20 dark:bg-zinc-900/80 hover:bg-zinc-200/20 dark:hover:bg-zinc-900/80 hover:text-zinc-700 dark:hover:text-white"
-    //   )}
-    // >
-    //   <div className="flex items-center gap-x-2 py-4">
-    //     <Icon
-    //       size={22}
-    //       className={cn(
-    //         "text-zinc-500 dark:text-white",
-    //         isActive && "text-zinc-700 dark:text-white"
-    //       )}
-    //     />
-    //     {label}
-    //   </div>
-    //   <div
-    //     className={cn(
-    //       "ml-auto opacity-0 border-2 border-zinc-70 dark:border-zinc-800 h-full transition-all",
-    //       isActive && "opacity-100"
-    //     )}
-    //   />
-    // </button>
     <Button
       onClick={onClick}
       type="button"
       variant="ghost"
       size="lg"
       className={cn(
-        "flex items-center justify-start p-2 hover:bg-zinc-200/50 dark:hover:bg-zinc-800/60 transition-all ease-in-out duration-200",
+        "flex items-center justify-start text-zinc-500 dark:text-zinc-200 hover:dark-white text-sm font-[500] transition-all hover:text-zinc-600 dark:hover:text-white hover:bg-zinc-300/20 dark:hover:bg-zinc-800/50 px-2",
         isActive &&
-          "bg-zinc-200/50 dark:bg-zinc-800/60 hover:bg-zinc-200/50 dark:hover:bg-zinc-800/60"
+          "text-zinc-700 dark:text-white bg-zinc-200/50 dark:bg-zinc-800/90 hover:bg-zinc-200/50 dark:hover:bg-zinc-800/90 hover:text-zinc-700 dark:hover:text-white"
       )}
     >
-      <div className="flex items-center gap-x-2 py-4">
+      <div className="flex items-center gap-x-2 hover:text-white">
         <Icon
           size={22}
           className={cn(
-            "text-zinc-500 dark:text-white",
+            "text-zinc-500 dark:text-zinc-200",
             isActive && "text-zinc-700 dark:text-white"
           )}
         />
         {label}
       </div>
-      {/* <div
-        className={cn(
-          "ml-auto opacity-0 border-2 border-zinc-70 dark:border-zinc-800 h-full transition-all"
-        )}
-      /> */}
     </Button>
   );
 };
