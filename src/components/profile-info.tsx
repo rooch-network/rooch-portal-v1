bc2c440dd443c2332e7a530b7828ed0dfba2f011
TwilightLogic <twilightlogic@gmail.com>
2024-02-11 11:26:36 +0800

    feat: add animations for profile

--- src/components/profile-info.tsx
+++ src/components/profile-info.tsx
@@ -6,7 +6,7 @@ export const ProfileInfo = () => {
     <Button
       variant="ghost"
       size="sm"
-      className="h-[55px] w-full cursor-pointer rounded-full flex items-center justify-start"
+      className="h-[55px] w-full cursor-pointer rounded-full flex items-center justify-start transition-all ease-in-out"
     >
       <div className="flex items-center justify-start gap-x-3">
         <Avatar>
