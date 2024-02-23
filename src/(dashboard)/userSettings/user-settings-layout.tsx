b21a71151423705c8bd73323ecfa2cdd23582534
TwilightLogic <twilightlogic@gmail.com>
2024-02-23 18:52:04 +0800

    feat: complete the layouts of address card on the `settings` page

--- src/(dashboard)/userSettings/user-settings-layout.tsx
+++ src/(dashboard)/userSettings/user-settings-layout.tsx
@@ -1,14 +1,46 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const UserSettingsLayout = () => {
   return (
-    <div className="h-full flex-1 flex-col space-y-8 flex">
-      <div className="flex items-center justify-between space-y-2">
-        <span>
-          <h1 className="text-3xl font-bold tracking-tight">Rooch Account</h1>
-          <p className="text-muted-foreground text-wrap">
-            Manage Your Wallet Connections and Authorized Sessions
-          </p>
-        </span>
-      </div>
+    <div className="h-full w-full flex-1 flex-col space-y-8 flex">
+      <Card className="relative overflow-hidden w-full border-border/40">
+        <CardHeader className="absolute top-0 left-0 z-10 p-4 md:p-6 w-full">
+          <div className="flex items-start justify-between">
+            <div>
+              <CardTitle className="text-2xl md:text-3xl leading-tight text-white">
+                Rooch Account
+              </CardTitle>
+              <CardDescription className="text-wrap text-white/85 dark:text-white/70 text-xs md:text-sm">
+                Manage Your Wallet Connections and Authorized Sessions.
+              </CardDescription>
+            </div>
+            <div className="ml-2">
+              <span>Your balance</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="p-0">
+          <div>
+            <div className="bg-gradient-to-r from-sky-500 to-indigo-500 object-cover w-full h-28 md:h-32 opacity-80 select-none" />
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between relative pb-8 md:pb-14 px-4 md:px-6 dark:bg-zinc-800/90 h-full">
+          <div className="absolute">
+            <Avatar className="w-12 h-12 md:w-20 md:h-20">
+              <AvatarImage src="https://github.com/shadcn.png" alt="Logic" />
+              <AvatarFallback className="text-xl">LO</AvatarFallback>
+            </Avatar>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
