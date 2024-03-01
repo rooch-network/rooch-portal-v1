5ab6c8af2b7f7624e6792b1650b180da3c7e83df
TwilightLogic <82878302+TwilightLogic@users.noreply.github.com>
2024-03-01 10:15:26 +0800

    refactor: use "pnpm"  (#31)
    * refactor: use "pnpm"

--- src/(dashboard)/components/main-content.tsx
+++ src/(dashboard)/components/main-content.tsx
@@ -5,6 +5,7 @@ import { UserMintLayout } from "../userMint/user-mint-layout";
 import { UserAppsLayout } from "../userApps/user-apps-layout";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { UserSettingsLayout } from "../userSettings/user-settings-layout";
+import { SftDetailLayout } from "../userMint/sftDetail/sft-detail-layout";
 
 export const MainContent = () => {
   return (
@@ -14,6 +15,7 @@ export const MainContent = () => {
         <Routes>
           <Route path="/" element={<UserAssetsLayout />} />
           <Route path="/mint" element={<UserMintLayout />} />
+          <Route path="/mint/sft/:sftId" element={<SftDetailLayout />} />
           <Route path="/apps" element={<UserAppsLayout />} />
           <Route path="/transactions" element={<UserTransactionsLayout />} />
           <Route path="/settings" element={<UserSettingsLayout />} />
--- src/(dashboard)/userApps/user-apps-layout.tsx
+++ src/(dashboard)/userApps/user-apps-layout.tsx
@@ -65,7 +65,7 @@ const mockApps = [
 
 export const UserAppsLayout = () => {
   return (
-    <div className="h-full flex-1 flex-col space-y-8 flex">
+    <div className="h-full flex-1 flex-col space-y-6 flex">
       <div className="flex items-center justify-between space-y-2">
         <span>
           <h1 className="text-3xl font-bold tracking-tight">Apps</h1>
