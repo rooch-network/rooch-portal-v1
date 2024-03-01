30c5f0b920c28620f8a8506af5ccfb78d94ca1f4
TwilightLogic <twilightlogic@gmail.com>
2024-03-01 18:31:59 +0800

    feat: complete the SFT details page

--- src/(dashboard)/userMint/sftDetail/components/sft-details.tsx
+++ src/(dashboard)/userMint/sftDetail/components/sft-details.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { useEffect, useState } from "react";
 
@@ -11,15 +19,75 @@ export function SftDetails() {
 
   return (
     <>
-      <div className="flex items-center justify-start w-full gap-2 text-muted-foreground">
+      <div className="flex items-center justify-start w-full gap-2 text-muted-foreground mt-2">
         <span className="text-sm">Process</span>
         <Progress value={progress} />
-        <span className="text-sm">84.4% Minted</span>
+        <span className="text-sm flex items-center gap-1">
+          <p className="font-semibold">84.4%</p>
+          <p className="text-muted-foreground/80">Minted</p>
+        </span>
       </div>
-      <div className="flex items-start justify-start gap-x-6 w-full">
-        <div className="w-1/2 h-full"></div>
-        <div className="w-1/2 h-full flex flex-col items-start justify-start"></div>
+      <div className="grid md:grid-cols-2 gap-x-6 w-full mt-6 gap-4 md:gap-6">
+        <div className="w-full h-full">
+          <Card className="border-border/40 shadow-lg">
+            <CardHeader>
+              <CardTitle>Overview</CardTitle>
+              <CardDescription>
+                Deploy your new project in one-click.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm text-muted-foreground grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="w-full h-full">
+          <Card className="border-border/40 shadow-lg">
+            <CardHeader>
+              <CardTitle>Progress</CardTitle>
+              <CardDescription>
+                Deploy your new project in one-click.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm text-muted-foreground grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+              <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
+                <span className="text-4xl font-medium md:text-5xl">12,333</span>
+                <span className="text-base md:text-sm">Distribution Type</span>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
+      <Button className="rounded-lg w-full mt-8 mb-6 h-12 bg-blue-900 hover:bg-blue-950 dark:text-white dark:bg-blue-800 dark:hover:bg-opacity-75">
+        Mint
+      </Button>
     </>
   );
 }
