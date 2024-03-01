ee0e9aae088c969f8ef6c75458cf1f59902aeeba
TwilightLogic <twilightlogic@gmail.com>
2024-03-01 10:21:12 +0800

    chore: remove the repetead importing package (force)

--- src/(dashboard)/userMint/user-mint-layout.tsx
+++ src/(dashboard)/userMint/user-mint-layout.tsx
@@ -1,7 +1,5 @@
 import { SftTabs } from "./components/sft-tabs";
 
-import { SftTabs } from "./components/sft-tabs";
-
 export const UserMintLayout = () => {
   return (
     <div className="h-full flex-1 flex-col space-y-6 flex">
