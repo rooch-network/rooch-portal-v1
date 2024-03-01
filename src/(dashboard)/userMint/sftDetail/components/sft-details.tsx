4efaaac55b314dcb307a52faf6180e34ad82d00a
TwilightLogic <twilightlogic@gmail.com>
2024-03-01 18:35:18 +0800

    feat: update some styles for SFT details page

--- src/(dashboard)/userMint/sftDetail/components/sft-details.tsx
+++ src/(dashboard)/userMint/sftDetail/components/sft-details.tsx
@@ -36,7 +36,7 @@ export function SftDetails() {
                 Deploy your new project in one-click.
               </CardDescription>
             </CardHeader>
-            <CardContent className="text-sm text-muted-foreground grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
+            <CardContent className="text-sm text-muted-foreground dark:text-primary grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
               <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
                 <span className="text-4xl font-medium md:text-5xl">12,333</span>
                 <span className="text-base md:text-sm">Distribution Type</span>
@@ -64,7 +64,7 @@ export function SftDetails() {
                 Deploy your new project in one-click.
               </CardDescription>
             </CardHeader>
-            <CardContent className="text-sm text-muted-foreground grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
+            <CardContent className="text-sm text-muted-foreground dark:text-primary grid md:grid-cols-2 gap-6 w-full px-6 md:px-0">
               <div className="flex flex-row-reverse md:flex-col items-center justify-between md:justify-center gap-2 w-full md:h-28">
                 <span className="text-4xl font-medium md:text-5xl">12,333</span>
                 <span className="text-base md:text-sm">Distribution Type</span>
@@ -85,7 +85,7 @@ export function SftDetails() {
           </Card>
         </div>
       </div>
-      <Button className="rounded-lg w-full mt-8 mb-6 h-12 bg-blue-900 hover:bg-blue-950 dark:text-white dark:bg-blue-800 dark:hover:bg-opacity-75">
+      <Button className="rounded-lg w-full mt-4 mb-2 md:mt-8 md:mb-6 h-12 bg-blue-900 hover:bg-blue-950 dark:text-white dark:bg-blue-800 dark:hover:bg-opacity-75">
         Mint
       </Button>
     </>
